87bab3d70634a2c40c529189749d865dd7127eaf
Lucas Zanella <lucasza.privado@gmail.com>
2024-10-08 23:56:50 -0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,135 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        do {
+            switch (menuInicial()){
+                case 0:
+                    System.exit(0);
+                break;
+                case 1:
+                    switch (subMenuCliente()){
+                        case 0:
+                            break;
+                        case 1:
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+                        break;
+                        case 2:
+
+                        break;
+                        case 3:
+
+                        break;
+                        default: System.out.println("\t\tDigite o respectivo número!!");
+                    }
+                break;
+                case 2:
+                    switch (subMenuPet()){
+                        case 0:
+                            break;
+                        case 1:
+
+                        break;
+                        case 2:
+
+                        break;
+                        default: System.out.println("\t\tDigite o respectivo número!!");
+                    }
+                break;
+                case 3:
+                    switch (subMenuServicos()){
+                        case 0:
+                            break;
+                        case 1:
+
+                        break;
+                        case 2:
+
+                        break;
+                        case 3:
+
+                        break;
+                        default: System.out.println("\t\tDigite o respectivo número!!");
+                    }
+                break;
+                default: System.out.println("\t\tDigite o respectivo número!!");
+            }
+        }while (true);
+    }
+
+    public static int menuInicial(){
+        Scanner menu_escolha = new Scanner(System.in);
+
+        System.out.println("=============================================");
+        System.out.println("\t\t\t  MENU DA APLICAÇÃO ");
+        System.out.println("=============================================");
+        System.out.println("1 -> Cadastro de clientes.");
+        System.out.println("2 -> Cadastro de pets.");
+        System.out.println("3 -> Serviços e controle de caixa.");
+        System.out.println("0 -> Sair do sistema.");
+        System.out.println("=============================================");
+        System.out.print("Digite sua opção:\t");
+
+        return menu_escolha.nextInt();
+    }
+
+    public static int subMenuCliente(){
+        Scanner menu_escolha = new Scanner(System.in);
+
+        System.out.println("=============================================");
+        System.out.println("\t    MENU DA APLICAÇÃO --> CLIENTES");
+        System.out.println("=============================================");
+        System.out.println("1. Cadastro de clientes.");
+        System.out.println("\t* 1 ->  Incluir clientes.");
+        System.out.println("\t* 2 ->  Listar clientes.");
+        System.out.println("\t* 3 ->  Excluir clientes.");
+        System.out.println("\t* 0 ->  Voltar.\n");
+        System.out.println("2. Cadastro de pets.");
+        System.out.println("3. Serviços e controle de caixa.");
+        System.out.println("0. Sair do sistema.");
+        System.out.println("=============================================");
+        System.out.print("Digite sua opção:\t");
+
+        return menu_escolha.nextInt();
+    }
+
+    public static int subMenuPet(){
+        Scanner menu_escolha = new Scanner(System.in);
+
+        System.out.println("=============================================");
+        System.out.println("\t\t  MENU DA APLICAÇÃO --> PETS");
+        System.out.println("=============================================");
+        System.out.println("1. Cadastro de clientes.");
+        System.out.println("2. Cadastro de pets.");
+        System.out.println("\t* 1 ->  Incluir pets.");
+        System.out.println("\t* 2 ->  Excluir pets.");
+        System.out.println("\t* 0 ->  Voltar.\n");
+        System.out.println("3. Serviços e controle de caixa.");
+        System.out.println("0. Sair do sistema.");
+        System.out.println("=============================================");
+        System.out.print("Digite sua opção:\t");
+
+        return menu_escolha.nextInt();
+    }
+
+    public static int subMenuServicos(){
+        Scanner menu_escolha = new Scanner(System.in);
+
+        System.out.println("=============================================");
+        System.out.println("\t\tMENU DA APLICAÇÃO --> SERVIÇOS");
+        System.out.println("=============================================");
+        System.out.println("1. Cadastro de clientes.");
+        System.out.println("2. Cadastro de pets.");
+        System.out.println("3. Serviços e controle de caixa.");
+        System.out.println("\t* 1 ->  Lançar serviço.");
+        System.out.println("\t* 2 ->  Lançar pagamento.");
+        System.out.println("\t* 3 ->  Listar saldo dos clientes.");
+        System.out.println("\t* 4 ->  Listar clientes com saldo negativo.");
+        System.out.println("\t* 0 ->  Voltar.\n");
+        System.out.println("0. Sair do sistema.");
+        System.out.println("=============================================");
+        System.out.print("Digite sua opção:\t");
+
+        return menu_escolha.nextInt();
     }
 }
